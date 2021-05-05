7c462b4a3dd6fe33d205b910e086f60468de7869
Johan-Kall <karl-johan.kall@studerande.plushogskolan.se>
2021-05-05 14:19:50 +0200

    johan's first code

--- cypress/integration/test-suite.spec.js
+++ cypress/integration/test-suite.spec.js
@@ -1,11 +1,21 @@
 /// <reference types="cypress" />
 
-describe('Assignment 02 - Karl-Johan Käll', function(){
-    /*beforeEach( ()=>{
-        cy.visit(
-    })*/
+import * as targets from '../targets/targets'
 
+describe('Assignment 03', function(){
     it('Perform Valid login', function(){
-        console.log("lala");
+        cy.visit(targets.base_url)
+        cy.performLogin(cy)
     })
 })
+
+function performLogin(cy){
+    cy.get(':nth-child(1) > input').type(targets/username)
+    cy.get(':nth-child(2) > input').type(targets/password)
+    cy.get('.btn').click()
+    cy.contains('Login')
+}
+
+module.exports = {
+    performLogin
+}
