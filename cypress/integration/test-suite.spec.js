6a1087f5eaee8d3bc3de5580eeced0ae226cf970
Johan-Kall <karl-johan.kall@studerande.plushogskolan.se>
2021-05-05 13:50:27 +0200

    target and test-suite

--- cypress/integration/test-suite.spec.js
+++ cypress/integration/test-suite.spec.js
@@ -1,1 +1,11 @@
-//lala
+/// <reference types="cypress" />
+
+describe('Assignment 02 - Karl-Johan Käll', function(){
+    /*beforeEach( ()=>{
+        cy.visit(
+    })*/
+
+    it('Perform Valid login', function(){
+        console.log("lala");
+    })
+})
